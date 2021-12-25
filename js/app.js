02c20d2ea0ea0108ec1b04da5cdf6578fbbf2ff8
MartaSolo <marciamarcia@o2.pl>
2021-12-25 12:17:01 +0100

    calculatin g prices, events on inputs

--- js/app.js
+++ js/app.js
@@ -2,77 +2,163 @@
 const inpProducts = document.querySelector("#products");
 const inpOrders = document.querySelector("#orders");
 const divPackage = document.querySelector("#package");
-const listPackage = divPackage.querySelector(".select__dropdown");
+const listPackage = document.querySelectorAll(".select__dropdown li");
 const inpAccounting = document.querySelector("#accounting");
 const inpTerminal = document.querySelector("#terminal");
 
 //--- Selecting elements of summary ---//
+
 // Products
 const products = document.querySelector("[data-id=products]");
 const productsNumber = products.querySelector(".item__calc");
 const productsPrice = products.querySelector(".item__price");
-console.log(products);
-console.log(productsNumber);
-console.log(productsPrice);
 
 // Orders
 const orders = document.querySelector("[data-id=orders]");
 const ordersNumber = orders.querySelector(".item__calc");
 const ordersPrice = orders.querySelector(".item__price");
-console.log(orders);
-console.log(ordersNumber);
-console.log(ordersPrice);
 
 //Package
 const package = document.querySelector("[data-id=package]");
-console.log(package);
+const packageType = package.querySelector(".item__calc");
+const packagePrice = package.querySelector(".item__price");
 
-const accounting = document.querySelector("[data-id=package]");
-console.log(accounting);
+//Accounting
+const accounting = document.querySelector("[data-id=accounting]");
+const accountingPrice = accounting.querySelector(".item__price");
+
+//Terminal
 const terminal = document.querySelector("[data-id=terminal]");
-console.log(terminal);
+const terminalPrice = terminal.querySelector(".item__price");
+
+// Total price
 const totalPrice = document.querySelector("#total-price");
-console.log(totalPrice);
+const totalPriceSum = totalPrice.querySelector(".total__price");
 
 //--- Object containing single prices ---//
 const prices = {
   product: 0.5,
   order: 0.25,
   package: {
-    bascic: 0,
+    basic: 0,
     professional: 25,
     premium: 60,
   },
   accounting: 10,
-  terminal: 10,
+  terminal: 5,
 };
-console.log(prices.product);
 
 //--- Event on products quantity ---//
-
 inpProducts.addEventListener("change", function () {
   products.classList.add("open");
   productsNumber.innerText = `${inpProducts.value} * $${prices.product}`;
   let totalProductPrice = inpProducts.value * prices.product;
   productsPrice.innerText = `$${totalProductPrice}`;
+  showSumPrice();
 });
 
 //--- Event on orders number ---//
-
 inpOrders.addEventListener("change", function () {
   orders.classList.add("open");
   ordersNumber.innerText = `${inpOrders.value} * $${prices.order}`;
   let totalOrderPrice = inpOrders.value * prices.order;
   ordersPrice.innerText = `$${totalOrderPrice}`;
+  showSumPrice();
 });
 
 //--- Event on package type ---//
+divPackage.addEventListener("click", function (e) {
+  divPackage.classList.toggle("open");
+  // e.target.dataset.value is a chosen li element
+  let value;
+  if (typeof e.target.dataset.value !== "undefined") {
+    value = e.target.dataset.value;
+  } else {
+    value = "";
+  }
 
-divPackage.addEventListener("click", function () {
-  listPackage.style.display = "block";
-  // jak zrobic wybieranie opcji z listy
-  // jak zrobic aby lista sie schowała?, jakies toggle?
-  // package.classList.toggle("open");
+  let text;
+  if (typeof e.target.dataset.value !== "undefined") {
+    text = e.target.innerText;
+  } else {
+    text = "";
+  }
+
+  if (value.length > 0) {
+    divPackage.dataset.value = value;
+    divPackage.querySelector(".select__input").innerText = text;
+  }
+
+  packageType.innerText = text;
+  let totalPackagePrice;
+  if (value === divPackage.dataset.value) {
+    totalPackagePrice = prices.package[divPackage.dataset.value];
+  } else if (value === "") {
+    totalPackagePrice = "";
+  }
+  packagePrice.innerText = `$${totalPackagePrice}`;
+
+  // opening summary for the package
+  for (let el of listPackage) {
+    el.addEventListener("click", function (e) {
+      package.classList.add("open");
+    });
+  }
+
+  showSumPrice();
 });
 
-// sprawdzenie wprowadzanej wartości do inputów??
+//--- Event on accounting ---//
+inpAccounting.addEventListener("change", function () {
+  accounting.classList.add("open");
+  accountingPrice.innerText = `$${prices.accounting}`;
+  showSumPrice();
+});
+
+//--- Event on Terminal ---//
+inpTerminal.addEventListener("change", function () {
+  terminal.classList.add("open");
+  terminalPrice.innerText = `$${prices.terminal}`;
+  showSumPrice();
+});
+
+//--- Function to calculate the total price ---//
+function sumPrice() {
+  let sum = 0;
+  // products
+  if (inpProducts.value > 0 && inpProducts.value % 1 === 0) {
+    sum += inpProducts.value * prices.product;
+  }
+  // orders
+  if (inpOrders.value > 0 && inpOrders.value % 1 === 0) {
+    sum += inpOrders.value * prices.order;
+  }
+  // package
+  for (el of listPackage) {
+    if (el.dataset.value === divPackage.dataset.value) {
+      sum += prices.package[divPackage.dataset.value];
+    }
+  }
+  // accounting
+  if (inpAccounting.checked) {
+    sum += prices.accounting;
+  }
+  // terminal
+  if (inpTerminal.checked) {
+    sum += prices.terminal;
+  }
+  return sum;
+}
+sumPrice();
+
+// Function showing the updated total price
+function showSumPrice() {
+  totalPrice.classList.add("open");
+  totalPriceSum.innerText = "$" + sumPrice();
+}
+showSumPrice();
+
+// jka zrobi, zeby po odznaczeniu checkboxów znikały zielone pola, bo kalkulator aktualizuje
+// git
+
+// potem stylowanie
