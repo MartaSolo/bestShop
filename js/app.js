b0284e21f3d9a67d240d1b464691afd04cd398ab
MartaSolo <marciamarcia@o2.pl>
2021-12-25 13:04:38 +0100

    editing the calculator

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
-//--- Selecting input elements ---//
+/*
+Function calculating the price. The price is automatically updated according to the values passed by aa user.
+
+Author: MartaSolo
+Date: 12.2021
+*/
+
+
+//--- Selecting elements ---//
+
+//Selecting input elements
 const inpProducts = document.querySelector("#products");
 const inpOrders = document.querySelector("#orders");
 const divPackage = document.querySelector("#package");
@@ -6,8 +16,7 @@ const listPackage = document.querySelectorAll(".select__dropdown li");
 const inpAccounting = document.querySelector("#accounting");
 const inpTerminal = document.querySelector("#terminal");
 
-//--- Selecting elements of summary ---//
-
+//Selecting elements of summary
 // Products
 const products = document.querySelector("[data-id=products]");
 const productsNumber = products.querySelector(".item__calc");
@@ -35,6 +44,7 @@ const terminalPrice = terminal.querySelector(".item__price");
 const totalPrice = document.querySelector("#total-price");
 const totalPriceSum = totalPrice.querySelector(".total__price");
 
+
 //--- Object containing single prices ---//
 const prices = {
   product: 0.5,
@@ -48,81 +58,9 @@ const prices = {
   terminal: 5,
 };
 
-//--- Event on products quantity ---//
-inpProducts.addEventListener("change", function () {
-  products.classList.add("open");
-  productsNumber.innerText = `${inpProducts.value} * $${prices.product}`;
-  let totalProductPrice = inpProducts.value * prices.product;
-  productsPrice.innerText = `$${totalProductPrice}`;
-  showSumPrice();
-});
+// --- Functions ---//
 
-//--- Event on orders number ---//
-inpOrders.addEventListener("change", function () {
-  orders.classList.add("open");
-  ordersNumber.innerText = `${inpOrders.value} * $${prices.order}`;
-  let totalOrderPrice = inpOrders.value * prices.order;
-  ordersPrice.innerText = `$${totalOrderPrice}`;
-  showSumPrice();
-});
-
-//--- Event on package type ---//
-divPackage.addEventListener("click", function (e) {
-  divPackage.classList.toggle("open");
-  // e.target.dataset.value is a chosen li element
-  let value;
-  if (typeof e.target.dataset.value !== "undefined") {
-    value = e.target.dataset.value;
-  } else {
-    value = "";
-  }
-
-  let text;
-  if (typeof e.target.dataset.value !== "undefined") {
-    text = e.target.innerText;
-  } else {
-    text = "";
-  }
-
-  if (value.length > 0) {
-    divPackage.dataset.value = value;
-    divPackage.querySelector(".select__input").innerText = text;
-  }
-
-  packageType.innerText = text;
-  let totalPackagePrice;
-  if (value === divPackage.dataset.value) {
-    totalPackagePrice = prices.package[divPackage.dataset.value];
-  } else if (value === "") {
-    totalPackagePrice = "";
-  }
-  packagePrice.innerText = `$${totalPackagePrice}`;
-
-  // opening summary for the package
-  for (let el of listPackage) {
-    el.addEventListener("click", function (e) {
-      package.classList.add("open");
-    });
-  }
-
-  showSumPrice();
-});
-
-//--- Event on accounting ---//
-inpAccounting.addEventListener("change", function () {
-  accounting.classList.add("open");
-  accountingPrice.innerText = `$${prices.accounting}`;
-  showSumPrice();
-});
-
-//--- Event on Terminal ---//
-inpTerminal.addEventListener("change", function () {
-  terminal.classList.add("open");
-  terminalPrice.innerText = `$${prices.terminal}`;
-  showSumPrice();
-});
-
-//--- Function to calculate the total price ---//
+// Function calculating the total price
 function sumPrice() {
   let sum = 0;
   // products
@@ -158,7 +96,79 @@ function showSumPrice() {
 }
 showSumPrice();
 
-// jka zrobi, zeby po odznaczeniu checkboxów znikały zielone pola, bo kalkulator aktualizuje
-// git
 
-// potem stylowanie
+
+//--- Events on inputs ---//
+
+// Event on products quantity
+inpProducts.addEventListener("change", function () {
+  products.classList.add("open");
+  productsNumber.innerText = `${inpProducts.value} * $${prices.product}`;
+  let totalProductPrice = inpProducts.value * prices.product;
+  productsPrice.innerText = `$${totalProductPrice}`;
+  showSumPrice();
+});
+
+// Event on orders number
+inpOrders.addEventListener("change", function () {
+  orders.classList.add("open");
+  ordersNumber.innerText = `${inpOrders.value} * $${prices.order}`;
+  let totalOrderPrice = inpOrders.value * prices.order;
+  ordersPrice.innerText = `$${totalOrderPrice}`;
+  showSumPrice();
+});
+
+// Event on package type
+divPackage.addEventListener("click", function (e) {
+  divPackage.classList.toggle("open");
+  // e.target.dataset.value is a chosen li element
+  let value;
+  if (typeof e.target.dataset.value !== "undefined") {
+    value = e.target.dataset.value;
+  } else {
+    value = "";
+  }
+
+  let text;
+  if (typeof e.target.dataset.value !== "undefined") {
+    text = e.target.innerText;
+  } else {
+    text = "";
+  }
+
+  if (value.length > 0) {
+    divPackage.dataset.value = value;
+    divPackage.querySelector(".select__input").innerText = text;
+  }
+  // passing values to the summary fields
+  packageType.innerText = text;
+  let totalPackagePrice;
+  if (value === divPackage.dataset.value) {
+    totalPackagePrice = prices.package[divPackage.dataset.value];
+  } else if (value === "") {
+    totalPackagePrice = "";
+  }
+  packagePrice.innerText = `$${totalPackagePrice}`;
+
+  // opening summary for the package
+  for (let el of listPackage) {
+    el.addEventListener("click", function (e) {
+      package.classList.add("open");
+    });
+  }
+  showSumPrice();
+});
+
+// Event on accounting
+inpAccounting.addEventListener("change", function () {
+  accounting.classList.toggle("open");
+  accountingPrice.innerText = `$${prices.accounting}`;
+  showSumPrice();
+});
+
+// Event on Terminal
+inpTerminal.addEventListener("change", function () {
+  terminal.classList.toggle("open");
+  terminalPrice.innerText = `$${prices.terminal}`;
+  showSumPrice();
+});
